ebf885b27e4458c840905f2d76d50b7245311d83
Václav Špaček <vaclavspacek@Vaclavs-MacBook-Pro.local>
2021-06-02 00:34:04 +0200

    Finish working

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,24 @@ const weatherForm = document.querySelector('form')
 
 const search = document.querySelector('input')
 
+const msgOne = document.querySelector('#msg1')
+const msgTwo = document.querySelector('#msg2')
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
 
+    msgOne.textContent = 'Loading...'
+    msgTwo.textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                msgOne.textContent = data.error
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                msgOne.textContent = data.location
+                msgTwo.textContent = data.forecast
             }
         })
     })
